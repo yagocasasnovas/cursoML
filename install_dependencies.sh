94076c76bd721c0519497d33410841680a33c07c
yago <islarobots@gmail.com>
2017-09-29 11:15:57 +0200

    s

---
+++ install_dependencies.sh
@@ -0,0 +1,17 @@
+touch ~/Escritorio/cursoML
+
+apt-get update
+
+#read -p "install apache"
+
+apt-get install python2.7 --assume-yes
+
+apt-get install python-pip --assume-yes
+
+apt-get install python-tk --assume-yes
+
+apt-get install build-essential python-dev python-setuptools python-numpy python-scipy libatlas-dev --assume-yes
+
+pip install -U scikit-learn
+
+pip install matplotlib
---
+++ uninstall_dependencies.sh
@@ -0,0 +1,19 @@
+touch ~/Escritorio/cursoML
+
+
+
+#read -p "install apache"
+
+
+
+pip uninstall scikit-learn
+
+pip uninstall matplotlib
+
+apt-get remove python-pip* --assume-yes
+
+apt-get remove python-tk* --assume-yes
+
+
+
+
